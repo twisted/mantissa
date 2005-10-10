ab9d8bf13c96d07f50c201e55db1c0452987caea
glyph <>
2005-10-10 22:56:52 +0000

    distutils magic and package version information

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+distobj = setup(
+    name="Mantissa",
+    version="0.1",
+    maintainer="Divmod, Inc.",
+    maintainer_email="support@divmod.org",
+    url="http://divmod.org/trac/wiki/MantissaProject",
+    license="MIT",
+    platforms=["any"],
+    description="A multiprotocol application deployment platform",
+    classifiers=(
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Internet"),
+
+    packages=['xmantissa',
+              'xmantissa.plugins',
+              'xmantissa.test',
+
+              'axiom.plugins'],
+
+    package_data={'xmantissa': ['static/*', 'themes/base/*']})
+
+from epsilon.setuphelper import regeneratePluginCache
+regeneratePluginCache(distobj)
---
+++ xmantissa/__init__.py
@@ -0,0 +1,5 @@
+# -*- test-case-name: xmantissa.test -*-
+
+from epsilon.versions import Version
+
+version = Version(__name__, 0, 1, 0)
