1612f4f05c21151c721307a674937c1196d0197c
moe <>
2006-01-19 23:03:00 +0000

    and the same thing for the person actions fragment

--- xmantissa/static/js/people.js
+++ xmantissa/static/js/people.js
@@ -37,19 +37,44 @@ Mantissa.People.InlinePerson = Nevow.Athena.Widget.subclass();
 
 Mantissa.People.InlinePerson.method('showActions',
     function(self, event) {
-        self.eventTarget = event.target;
-        self.eventTarget.onclick = function() { self.hideActions(); return false };
 
         self.personActions = self.nodeByAttribute('class', 'person-actions');
         self.personActions.style.top = event.pageY;
         self.personActions.style.left = event.pageX;
 
         MochiKit.DOM.showElement(self.personActions);
+
+        self.eventTarget = event.target;
+        self.eventTarget.onclick = function() {
+            self.hideActions();
+            return false;
+        }
+
+        var body = document.getElementsByTagName("body")[0];
+        body.onclick = function(_event) {
+            if(event.target == _event.target) {
+                return false;
+            }
+            var e = _event.target;
+            while(e && e != self.node) {
+                e = e.parentNode;
+            }
+            if(e) {
+                return false;
+            }
+            self.hideActions();
+            body.onclick = null;
+            return false;
+        }
     });
 
 Mantissa.People.InlinePerson.method('hideActions',
     function(self) {
-        self.eventTarget.onclick = function(event) { self.showActions(event); return false };
+        self.eventTarget.onclick = function(event) {
+            self.showActions(event);
+            return false;
+        }
+            
         MochiKit.DOM.hideElement(self.personActions);
     });
 
