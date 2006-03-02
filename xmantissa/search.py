c367f5efadbf666417342641385a9c0f4c95de6a
exarkun <>
2006-03-02 20:17:30 +0000

    Merge indexer-subprocess-655-3 (no, seriously)
    Author: washort, exarkun
    Reviewer: moe
    Fixes #655
    
    This adds a child process, managed by a service automatically associated
    on-disk Stores, started when the Store service is started, stopped when it is
    stopped, accessible by adapting a Store to axiom.iaxiom.IBatchService, which
    processes certain kinds of batch tasks.
    
    Process management code is factored out of Sine and into a generally usable set
    of classes provided by Axiom.
    
    The hype indexer is changed to declare its work as out-of-process. Messages are
    passed between the two processes to coordinate searches, due to the constraints
    hype places on concurrent access to the index.
    
    Historical unit tests are cleaned up to use setUp and tearDown properly, so as
    not to start services but never stop them. Additionally, the code is now
    essentially identical across all of the historical tests, making it extremely
    ripe for factoring out.
    
    The API for reliable listeners is reified as axiom.iaxiom.IReliableListener and
    expanded to support suspend and resume messages.
    
    The search interface defined by mantissa is now asynchronous, requiring
    Deferred returns from count and search. ClickChronicle's xapian search-provider
    has been updated to comply with this.
    
    Several debugging aids useful during the course of development of this branch
    have been added. epsilon.spewer is a prettier version of
    twisted.python.util.spewer. JuiceBoxes? have a prettier repr() string.

--- xmantissa/search.py
+++ xmantissa/search.py
@@ -1,16 +1,26 @@
+# -*- test-case-name: xmantissa.test.test_search -*-
+
 from __future__ import division
+
+from operator import attrgetter
+
 from zope.interface import implements
+
+from twisted.internet import defer
+from twisted.python import components, log
+
+from nevow import rend, inevow, tags, flat
+from nevow.url import URL
+
+from epsilon.extime import Time
+
+from axiom import attributes
+from axiom.item import Item, InstallableMixin
 
 from xmantissa.ixmantissa import (ISearchAggregator, ISearchProvider,
                                   INavigableElement, INavigableFragment,
                                   IPreferenceAggregator)
-from axiom import attributes
-from axiom.item import Item, InstallableMixin
-from operator import attrgetter
-from nevow import rend, inevow, tags, flat
-from epsilon.extime import Time
-from nevow.url import URL
-from twisted.python.components import registerAdapter
+
 
 flat.registerFlattener(lambda t, ign: t.asHumanly(), Time)
 
@@ -41,28 +51,45 @@ class SearchAggregator(Item, InstallableMixin):
         other.powerUp(self, ISearchAggregator)
         other.powerUp(self, INavigableElement)
 
+
     def activate(self):
         self._searchProviders = None
 
+
     def _cachePowerups(self):
         self._searchProviders = list(self.installedOn.powerupsFor(ISearchProvider))
+
 
     # INavigableElement
     def getTabs(self):
         return []
 
+
     # ISearchAggregator
     def providers(self):
         if self._searchProviders is None:
             self._cachePowerups()
-
         return len(self._searchProviders)
+
 
     def count(self, term):
         if self._searchProviders is None:
             self._cachePowerups()
 
-        return max(provider.count(term) for provider in self._searchProviders)
+        def countedHits(results):
+            total = 0
+            for (success, result) in results:
+                if success:
+                    total += result
+                else:
+                    log.err(result)
+            return total
+
+        return defer.DeferredList([
+            provider.count(term)
+            for provider
+            in self._searchProviders], consumeErrors=True).addCallback(countedHits)
+
 
     def search(self, term, count, offset):
         self.searches += 1
@@ -70,14 +97,25 @@ class SearchAggregator(Item, InstallableMixin):
         if self._searchProviders is None:
             self._cachePowerups()
 
-        results = []
-        for provider in self._searchProviders:
-            results.extend(provider.search(term, count, offset))
+        d = defer.DeferredList([
+            provider.search(term, count, offset)
+            for provider in self._searchProviders
+            ], consumeErrors=True)
 
-        results.sort(key=attrgetter('score'))
-        results.reverse()
+        def searchCompleted(results):
+            allSearchResults = []
+            for (success, result) in results:
+                if success:
+                    allSearchResults.extend(result)
+                else:
+                    log.err(result)
 
-        return results
+            allSearchResults.sort(key=attrgetter('score'))
+            allSearchResults.reverse()
+            return allSearchResults
+        return d.addCallback(searchCompleted)
+
+
 
 class SearchAggregatorFragment(rend.Fragment):
     implements(INavigableFragment)
@@ -99,40 +137,58 @@ class SearchAggregatorFragment(rend.Fragment):
         prefAggregator = IPreferenceAggregator(original.installedOn)
         self.itemsPerPage = prefAggregator.getPreference('itemsPerPage').value
 
+
     def head(self):
         return tags.script(type='text/javascript',
                            src='/Mantissa/js/search.js')
 
+
     def setSearchTerm(self, ctx):
         qargs = dict(URL.fromContext(ctx).queryList())
         self.searchTerm = qargs['term']
-        self.totalItems = self.searchAggregator.count(self.searchTerm)
         self.currentPage = 1
 
+        def countedHits(hitCount):
+            self.totalItems = hitCount
+        return self.searchAggregator.count(self.searchTerm).addCallback(countedHits)
+
+
     def beforeRender(self, ctx):
-        self.setSearchTerm(ctx)
+        return self.setSearchTerm(ctx)
+
 
     def data_searchTerm(self, ctx, data):
         return self.searchTerm
 
+
     def makeResults(self):
-        data = self.searchAggregator.search(self.searchTerm,
-                                            self.itemsPerPage,
-                                            (self.currentPage-1) * self.itemsPerPage)
+        def toDict(result):
+            return dict(
+                url=result.url,
+                description=result.description,
+                score="%.1f" % (result.score * 100),
+                summary=result.summary, timestamp=result.timestamp)
 
-        todict = lambda sr: dict(url=sr.url, description=sr.description,
-                                 score="%.1f" % (sr.score * 100),
-                                 summary=sr.summary, timestamp=sr.timestamp)
+        d = self.searchAggregator.search(
+            self.searchTerm,
+            self.itemsPerPage,
+            (self.currentPage - 1) * self.itemsPerPage)
 
-        return (len(data), self.resultsPattern(data=map(todict, data)))
+        def searchCompleted(results):
+            return (len(results),
+                    self.resultsPattern(data=map(toDict, results)))
+        return d.addCallback(searchCompleted)
+
 
     def goingLive(self, ctx, client):
         self.resultsPattern = inevow.IQ(self.docFactory).patternGenerator('results')
-        (hits, data) = self.makeResults()
 
-        start = (self.currentPage - 1) * self.itemsPerPage
-        # (start, stop, total)
-        client.call('setSearchState', start+1, start+hits, self.totalItems)
-        client.set('results', data)
+        def madeResults((hits, data)):
+            start = (self.currentPage - 1) * self.itemsPerPage
+            # (start, stop, total)
+            client.call('setSearchState', start+1, start+hits, self.totalItems)
+            client.set('results', data)
 
-registerAdapter(SearchAggregatorFragment, SearchAggregator, INavigableFragment)
+        self.makeResults().addCallback(madeResults)
+
+components.registerAdapter(SearchAggregatorFragment, SearchAggregator, INavigableFragment)
