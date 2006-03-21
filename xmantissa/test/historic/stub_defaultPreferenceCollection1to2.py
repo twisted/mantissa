c7a40eda4a3ebc5d3c384f22072a59ddf90fcbac
glyph <>
2006-03-21 22:35:27 +0000

    Refactor historical test framework code like it said we were supposed to 20 tests ago.
    Author: glyph
    Reviewer: exarkun
    
    Re #225
    
    Doesn't actually close a new ticket :-(

--- xmantissa/test/historic/stub_defaultPreferenceCollection1to2.py
+++ xmantissa/test/historic/stub_defaultPreferenceCollection1to2.py
@@ -1,29 +1,10 @@
-import os
-import shutil
-import tarfile
-
-from axiom.store import Store
 
 from xmantissa import prefs
 
 def createDatabase(s):
     prefs.DefaultPreferenceCollection(store=s)
 
-# Below here should eventually be framework code, and is in fact identical to
-# the existing test in axiom.test.historic.  Refactor after this branch is
-# merged.
-
-def determineFile(f):
-    return os.path.join(
-        os.path.dirname(f),
-        os.path.basename(f).split("stub_")[1].split('.py')[0]+'.axiom')
+from axiom.test.historic.stubloader import saveStub
 
 if __name__ == '__main__':
-    dbfn = determineFile(__file__)
-    s = Store(dbfn)
-    s.transact(createDatabase, s)
-    s.close()
-    tarball = tarfile.open(dbfn+'.tbz2', 'w:bz2')
-    tarball.add(os.path.basename(dbfn))
-    tarball.close()
-    shutil.rmtree(dbfn)
+    saveStub(createDatabase)
--- xmantissa/test/historic/stub_freeTicketSignup2to3.py
+++ xmantissa/test/historic/stub_freeTicketSignup2to3.py
@@ -1,8 +1,5 @@
-import os
-import shutil
-import tarfile
 
-from axiom.store import Store
+from axiom.test.historic.stubloader import saveStub
 
 from xmantissa.signup import FreeTicketSignup
 
@@ -12,21 +9,5 @@ def createDatabase(s):
                      booth=s,
                      benefactor=s)
 
-# Below here should eventually be framework code, and is in fact identical to
-# the existing test in axiom.test.historic.  Refactor after this branch is
-# merged.
-
-def determineFile(f):
-    return os.path.join(
-        os.path.dirname(f),
-        os.path.basename(f).split("stub_")[1].split('.py')[0]+'.axiom')
-
 if __name__ == '__main__':
-    dbfn = determineFile(__file__)
-    s = Store(dbfn)
-    s.transact(createDatabase, s)
-    s.close()
-    tarball = tarfile.open(dbfn+'.tbz2', 'w:bz2')
-    tarball.add(os.path.basename(dbfn))
-    tarball.close()
-    shutil.rmtree(dbfn)
+    saveStub(createDatabase)
