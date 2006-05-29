de60deac421e2e398b6bf7ff8cecbabf911a126d
mithrandi <>
2006-05-29 16:34:24 +0000

    Merge js-modules-651.
    Fixes #651
    Author: mithrandi
    Reviewer: exarkun
    
    This branch implements {{{athena.AutoJSPackage}}} for use in nevow plugins; it
    automatically derives JS module mappings from the filesystem hierarchy, in a
    manner similar to Python. Unlike python, __init__.js files are optional, and
    will be treated like an empty file if not present. Mantissa and Quotient are
    also migrated to use the new mechanism.

--- nevow/plugins/mantissa_package.py
+++ nevow/plugins/mantissa_package.py
@@ -5,28 +5,4 @@ from nevow import athena
 
 import xmantissa
 
-def _f(*sibling):
-    return util.sibpath(xmantissa.__file__, '/'.join(sibling))
-
-mantissaPkg = athena.JSPackage({
-    'PlotKit.Base': _f('static', 'js', 'PlotKit', 'Base.js'),
-    'PlotKit.Canvas': _f('static', 'js', 'PlotKit', 'Canvas.js'),
-    'PlotKit.Layout': _f('static', 'js', 'PlotKit', 'Layout.js'),
-    'PlotKit.SVGRenderer': _f('static', 'js', 'PlotKit', 'SVG.js'),
-    'PlotKit.SweetSVG': _f('static', 'js', 'PlotKit', 'SweetSVG.js'),
-    'PlotKit.Canvas': _f('static', 'js', 'PlotKit', 'Canvas.js'),
-    'PlotKit.SweetCanvas': _f('static', 'js', 'PlotKit', 'SweetCanvas.js'),
-    'Mantissa': _f('static', 'js', 'mantissa.js'),
-    'Mantissa.Admin': _f('static', 'js', 'admin.js'),
-    'Mantissa.LiveForm': _f('static', 'js', 'liveform.js'),
-    'Mantissa.Validate': _f('static', 'js', 'validate.js'),
-    'Mantissa.People': _f('static', 'js', 'people.js'),
-    'Mantissa.TDB': _f('static', 'js', 'tdb.js'),
-    'Mantissa.Offering': _f('static', 'js', 'offerings.js'),
-    'Mantissa.Preferences': _f('static', 'js', 'preferences.js'),
-    'Mantissa.Authentication': _f('static', 'js', 'authentication.js'),
-    'Fadomatic': _f('static', 'js', 'fadomatic.js'),
-    'Mantissa.Test': _f('test', 'livetest.js'),
-    'Mantissa.ScrollTable': _f('static', 'js', 'scrolltable.js'),
-    'Mantissa.StatGraph': _f('static', 'js', 'statgraph.js'),
-    })
+mantissaPkg = athena.AutoJSPackage(util.sibpath(xmantissa.__file__, 'js'))
