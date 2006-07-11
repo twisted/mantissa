b55993c9822d50863db107b5b154e397384a65c4
moe <>
2006-07-11 18:20:04 +0000

    make MessageFader.start() return the deferred

--- xmantissa/js/Mantissa/LiveForm.js
+++ xmantissa/js/Mantissa/LiveForm.js
@@ -86,7 +86,7 @@ Mantissa.LiveForm.MessageFader.methods(
      */
     function start(self) {
         // kick off the timer loop
-        self.fadeIn().addCallback(function() { return self.fadeOut(); });
+        return self.fadeIn().addCallback(function() { return self.fadeOut(); });
     });
 
 
